d88d5a678d1376088186b0cdb5a47056785d14fe
Trey Tallent <treydentallent@gmail.com>
2024-10-20 13:00:23 +0800

    Tabs: adding a new tab adds a new panel

--- src/tabs/tab/index.js
+++ src/tabs/tab/index.js
@@ -1,21 +1,49 @@
 import { registerBlockType } from "@wordpress/blocks"
 import { useBlockProps } from "@wordpress/block-editor"
+import { createBlock } from "@wordpress/blocks"
 import { useEffect } from "@wordpress/element"
 import "./style.css"
 import "./editor.css"
 import metadata from "./block.json"
+import { store as blockEditorStore } from "@wordpress/block-editor"
+import { useDispatch } from "@wordpress/data"
 
 registerBlockType(metadata.name, {
    edit: props => {
-      // Sets the attribute id equal to the index value of a tab within it's parent tab-list
-      useEffect(() => {
-         if (!props.attributes.id === undefined) return
-         const indexValue = wp.data
-            .select("core/block-editor")
-            .getBlockIndex(props.clientId, ["artedwa-blocks/tab"])
+      const { insertBlock } = useDispatch(blockEditorStore)
+      const indexValue = wp.data
+         .select("core/block-editor")
+         .getBlockIndex(props.clientId, ["artedwa-blocks/tab"])
 
+      useEffect(() => {
+         console.log("running useeffect")
+         // Sets the attribute Id to it's index value in the parent array
+         if (!props.attributes.id === undefined) return
+         setId()
+
+         // Places a new tab panel when a new tab is inserted
+         const justInserted = wp.data
+            .select("core/block-editor")
+            .wasBlockJustInserted(props.clientId)
+
+         if (!justInserted) return
+         addTabPanel()
+      }, [props.clientId])
+
+      function addTabPanel() {
+         const panelsListClientId = wp.data
+            .select("core/block-editor")
+            .getBlocksByName("artedwa-blocks/panels-list")
+
+         const newPane = createBlock("artedwa-blocks/panel", {
+            id: indexValue,
+         })
+         insertBlock(newPane, indexValue, panelsListClientId[0])
+      }
+
+      function setId() {
          props.setAttributes({ id: indexValue })
-      })
+      }
 
       const blockProps = useBlockProps()
       return <button {...blockProps}>Button</button>
