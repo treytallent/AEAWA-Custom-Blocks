8c3e36ecc9c9712a79737628f70a23cc2be24abc
Trey Tallent <treydentallent@gmail.com>
2024-10-20 16:04:02 +0800

    Tabs: input changes tab name

--- src/tabs/tab/index.js
+++ src/tabs/tab/index.js
@@ -1,34 +1,36 @@
-import { registerBlockType } from "@wordpress/blocks"
-import { useBlockProps } from "@wordpress/block-editor"
-import { createBlock } from "@wordpress/blocks"
+import {
+   useBlockProps,
+   store as blockEditorStore,
+} from "@wordpress/block-editor"
+import { useDispatch } from "@wordpress/data"
+import { createBlock, registerBlockType } from "@wordpress/blocks"
 import { useEffect } from "@wordpress/element"
+
 import "./style.css"
 import "./editor.css"
 import metadata from "./block.json"
-import { store as blockEditorStore } from "@wordpress/block-editor"
-import { useDispatch } from "@wordpress/data"
 
 registerBlockType(metadata.name, {
-   edit: props => {
+   edit: ({ attributes, setAttributes, clientId }) => {
+      const { title, id } = attributes
       const { insertBlock } = useDispatch(blockEditorStore)
+
       const indexValue = wp.data
          .select("core/block-editor")
-         .getBlockIndex(props.clientId, ["artedwa-blocks/tab"])
+         .getBlockIndex(clientId, ["artedwa-blocks/tab"])
 
       useEffect(() => {
-         console.log("running useeffect")
          // Sets the attribute Id to it's index value in the parent array
-         if (!props.attributes.id === undefined) return
+         if (!id === undefined) return
          setId()
 
          // Places a new tab panel when a new tab is inserted
          const justInserted = wp.data
             .select("core/block-editor")
-            .wasBlockJustInserted(props.clientId)
-
+            .wasBlockJustInserted(clientId)
          if (!justInserted) return
          addTabPanel()
-      }, [props.clientId])
+      }, [clientId])
 
       function addTabPanel() {
          const panelsListClientId = wp.data
@@ -42,11 +44,22 @@ registerBlockType(metadata.name, {
       }
 
       function setId() {
-         props.setAttributes({ id: indexValue })
+         setAttributes({ id: indexValue })
       }
 
       const blockProps = useBlockProps()
-      return <button {...blockProps}>Button</button>
+
+      return (
+         <div {...blockProps}>
+            <input
+               {...blockProps}
+               value={attributes.title}
+               onChange={e =>
+                  setAttributes((attributes.title = e.target.value))
+               }
+            ></input>
+         </div>
+      )
    },
    save: () => {
       const blockProps = useBlockProps.save()
